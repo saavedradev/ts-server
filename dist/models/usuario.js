211fbee4d5c7d0b1d8b135b406a3d02bc2f279d1
esaave3 <esaave3@bancodebogota.com.co>
2021-08-09 17:33:16 -0500

    changes for user model

---
+++ dist/models/usuario.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const connection_1 = __importDefault(require("../db/connection"));
+const Usuario = connection_1.default.define('Usuario', {});
+//# sourceMappingURL=usuario.js.map
